f1a2b1dbdc56e6d2d8d9dd14f5997696eb1ad3ae
mabako <mabako@gmail.com>
2011-09-14 01:38:18 +0200

    changed lines for routes as to match the comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 LSMap::Application.routes.draw do
   devise_for :users
   
-  resources :points
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -16,6 +14,7 @@ LSMap::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :points
 
   # Sample resource route with options:
   #   resources :products do
@@ -53,12 +52,7 @@ LSMap::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "map#index"
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
-  
-  root :to => "map#index"
 end
