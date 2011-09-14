3748aabad0375ca9f14c0fe7fbd4c43d6aca05c2
mabako <mabako@gmail.com>
2011-09-14 15:45:58 +0200

    added validation for points (requires title, x, y & icon to be set)

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,9 @@
 class Point < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :x
+  validates_presence_of :y
+  validates_presence_of :icon
+  
+  validates_numericality_of :x, :greater_than_or_equal_to => -3000, :less_than_or_equal_to => 3000
+  validates_numericality_of :y, :greater_than_or_equal_to => -3000, :less_than_or_equal_to => 3000
 end
