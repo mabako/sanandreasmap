2bc809f0d09ac0c57dc4944e7397bbc63423a1b1
mabako <mabako@gmail.com>
2011-09-18 00:30:30 +0200

    HTTP Strict Transport Security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :use_ssl if Rails.env.production?
+ 
+private
+  def use_ssl
+    if request.ssl?
+      response.headers['Strict-Transport-Security'] = 'max-age=1234567890'
+    else
+      redirect_to "https://" + request.host + request.request_uri, :status => 301
+    end
+  end
 end
