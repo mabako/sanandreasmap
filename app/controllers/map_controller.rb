6274eecd1bd4f6af9a79dc55dfbde0996c49397a
mabako <mabako@gmail.com>
2011-09-17 20:44:20 +0200

    use api-key depending on whether http or https is used

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,10 @@
 class MapController < ApplicationController
   def index
+     if request.ssl?
+       @api_key = 'ABQIAAAAMWRZBB5vEAlPU51oiMzCwRQtfFQHhrL-D7NtvvPYCl3EcDEs1hQG6QNBVp3YL8FKRdaQoz6wolTnuw'
+     else
+       @api_key = 'ABQIAAAAMWRZBB5vEAlPU51oiMzCwRSwqdAaTweww-pAVZHjK8FxllAlEhRhqDskVzwJrPLrmiXKGDdVAHXm6A'
+     end
      render :layout => false
   end
 end
